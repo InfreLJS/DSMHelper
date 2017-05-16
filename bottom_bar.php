ef146b5c94037d896fac472a044b0b8c1ea32ef0
Jaeseok Lee <jaeseoklee@Jaeseokui-MBP.connectify>
2017-05-16 22:26:01 +0900

    bottom_bar assignment sync with homepage

--- bottom_bar.php
+++ bottom_bar.php
@@ -12,27 +12,82 @@
     <div id="bottom_bar_month">1 Month</div>
 </div>
 
-<div id="topics" class="outer">
+<div id="topics">
     <?php
-        $sql = "SELECT assignment.id, assignment.description, subject.name, CONCAT(until, ' ', period.start) AS until, TIMESTAMPDIFF(SECOND, NOW(), CONCAT(until, ' ', period.start)) AS untilt FROM assignment LEFT JOIN timetable ON assignment.timetable=timetable.id LEFT JOIN subject ON timetable.subject=subject.id LEFT JOIN period ON timetable.period=period.id WHERE TIMESTAMPDIFF(SECOND, NOW(), CONCAT(until, ' ', period.start)) <= (30*86400) AND TIMESTAMPDIFF(SECOND, NOW(), CONCAT(until, ' ', period.start)) >= 0";
+        $sql = "SELECT assignment.id, assignment.description, subject.name, CONCAT(assignment.untildate, ' ', period.start) AS until, TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', period.start)) AS untilt FROM assignment LEFT JOIN timetable ON assignment.timetable=timetable.id LEFT JOIN subject ON assignment.subject=subject.id LEFT JOIN period ON timetable.period=period.id WHERE TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', period.start)) <= (30*86400) AND TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', period.start)) >= 0 AND assignment.timeset = 1";
         $result = mysqli_query($conn, $sql);
         while ($row = mysqli_fetch_assoc($result)) {
-            echo '<div id="topic'.$row['id'].'" class="inner" style="
-                position: fixed;
-                top: 68%;
-                left: '.(((double)$row['untilt']/(30*86400))*90+5).'%;
-                width: 21%;
-                height: 25%;
-                border: 1px solid red;
-                border-radius: 10px;
-                background-color: white;
-                visibility: hidden;"><p class="centered">과목 : '.$row['name'].'<br>내용 : '.$row['description'].'<br>기한 : '.date("Y년 m월 d일 A h시 i분 까지", strtotime($row['until'])).'</p>
-            </div>';
+            if ((((double)$row['untilt']/(30*86400))*90+5) >= 80) {
+                echo '<div id="topic'.$row['id'].'" class="inner" style="
+                    position: fixed;
+                    top: 68%;
+                    left: '.(((double)$row['untilt']/(30*86400))*90-14).'%;
+                    width: 21%;
+                    height: 25%;
+                    border: 1px solid red;
+                    border-radius: 10px;
+                    background-color: white;
+                    visibility: hidden;"><p class="centered">과목 : '.$row['name'].'<br><br>'.$row['description'].'<br><br>기한 : '.date("Y년 m월 d일 A h시 i분 까지", strtotime($row['until'])).'</p>
+                </div>';
+            } else {
+                echo '<div id="topic'.$row['id'].'" class="inner" style="
+                    position: fixed;
+                    top: 68%;
+                    left: '.(((double)$row['untilt']/(30*86400))*90+5).'%;
+                    width: 21%;
+                    height: 25%;
+                    border: 1px solid red;
+                    border-radius: 10px;
+                    background-color: white;
+                    visibility: hidden;"><p class="centered">과목 : '.$row['name'].'<br><br>'.$row['description'].'<br><br>기한 : '.date("Y년 m월 d일 A h시 i분 까지", strtotime($row['until'])).'</p>
+                </div>';
+            }
+        }
+        $sql2 = "SELECT assignment.id, assignment.description, subject.name, CONCAT(assignment.untildate, ' ', assignment.untiltime) AS until, TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', assignment.untiltime)) AS untilt FROM assignment LEFT JOIN timetable ON assignment.timetable=timetable.id LEFT JOIN subject ON assignment.subject=subject.id WHERE TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', assignment.untiltime)) <= (30*86400) AND TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', assignment.untiltime)) >= 0 AND assignment.timeset = 0";
+        $result = mysqli_query($conn, $sql2);
+        while ($row = mysqli_fetch_assoc($result)) {
+            if ((((double)$row['untilt']/(30*86400))*90+5) >= 80) {
+                echo '<div id="topic'.$row['id'].'" class="inner" style="
+                    position: fixed;
+                    top: 68%;
+                    left: '.(((double)$row['untilt']/(30*86400))*90-14).'%;
+                    width: 21%;
+                    height: 25%;
+                    border: 1px solid red;
+                    border-radius: 10px;
+                    background-color: white;
+                    visibility: hidden;"><p class="centered">과목 : '.$row['name'].'<br><br>'.$row['description'].'<br><br>기한 : '.date("Y년 m월 d일 A h시 i분 까지", strtotime($row['until'])).'</p>
+                </div>';
+            } else {
+                echo '<div id="topic'.$row['id'].'" class="inner" style="
+                    position: fixed;
+                    top: 68%;
+                    left: '.(((double)$row['untilt']/(30*86400))*90+5).'%;
+                    width: 21%;
+                    height: 25%;
+                    border: 1px solid red;
+                    border-radius: 10px;
+                    background-color: white;
+                    visibility: hidden;"><p class="centered">과목 : '.$row['name'].'<br><br>'.$row['description'].'<br><br>기한 : '.date("Y년 m월 d일 A h시 i분 까지", strtotime($row['until'])).'</p>
+                </div>';
+            }
         }
         echo '</div>';
-        $sql = "SELECT assignment.id, assignment.description, subject.name, CONCAT(until, ' ', period.start) AS until, TIMESTAMPDIFF(SECOND, NOW(), CONCAT(until, ' ', period.start)) AS untilt FROM assignment LEFT JOIN timetable ON assignment.timetable=timetable.id LEFT JOIN subject ON timetable.subject=subject.id LEFT JOIN period ON timetable.period=period.id WHERE TIMESTAMPDIFF(SECOND, NOW(), CONCAT(until, ' ', period.start)) <= (30*86400) AND TIMESTAMPDIFF(SECOND, NOW(), CONCAT(until, ' ', period.start)) >= 0";
         $result = mysqli_query($conn, $sql);
         echo '<div id="dots">';
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<div id="dot'.$row['id'].'" style="
+                position: fixed;
+                bottom: 20px;
+                left: '.(((double)$row['untilt']/(30*86400))*90+5).'%;
+                width: 20px;
+                height: 20px;
+                border: 1px solid red;
+                background-color: red;
+                border-radius: 20px;
+                z-index: 101;"></div>';
+        }
+        $result = mysqli_query($conn, $sql2);
         while ($row = mysqli_fetch_assoc($result)) {
             echo '<div id="dot'.$row['id'].'" style="
                 position: fixed;
--- script.php
+++ script.php
@@ -1,11 +1,21 @@
-<link href="style.css?ver=1.2" rel="stylesheet">
+<link href="style.css?ver=1.5" rel="stylesheet">
 <link href="./bootstrap-3.3.4-dist/css/bootstrap.min.css?ver=0.1" rel="stylesheet">
 <script src="./bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 <script src="./jquery-3.2.0.min.js"></script>
 <script>
     $(document).ready(function() {
         <?php
-            $sql = "SELECT *, TIMESTAMPDIFF(SECOND, NOW(), until) AS untilt FROM homework WHERE TIMESTAMPDIFF(SECOND, NOW(), until) <= (30*86400)";
+            $sql = "SELECT assignment.id FROM assignment LEFT JOIN timetable ON assignment.timetable=timetable.id LEFT JOIN period ON timetable.period=period.id WHERE TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', period.start)) <= (30*86400) AND TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', period.start)) >= 0 AND assignment.timeset = 1";
+            $result = mysqli_query($conn, $sql);
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '$("div#dot'.$row['id'].'").mouseover(function(){
+                        $("div#topic'.$row['id'].'").css("visibility", "visible");
+                    });
+                    $("div#dot'.$row['id'].'").mouseout(function(){
+                        $("div#topic'.$row['id'].'").css("visibility", "hidden");
+                    });';
+            }
+            $sql = "SELECT assignment.id FROM assignment WHERE TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', assignment.untiltime)) <= (30*86400) AND TIMESTAMPDIFF(SECOND, NOW(), CONCAT(assignment.untildate, ' ', assignment.untiltime)) >= 0 AND assignment.timeset = 0";
             $result = mysqli_query($conn, $sql);
             while ($row = mysqli_fetch_assoc($result)) {
                 echo '$("div#dot'.$row['id'].'").mouseover(function(){
